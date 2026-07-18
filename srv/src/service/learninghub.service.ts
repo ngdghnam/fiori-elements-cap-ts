b836974397e098f5ca4633b34eec5f5e51cfa386
Nam Nguyen <stghoainam4002@gmail.com>
2026-07-18 10:47:01 +0700

    Resolve mismatch dependencies

--- srv/src/service/learninghub.service.ts
+++ srv/src/service/learninghub.service.ts
@@ -1,12 +1,20 @@
 import { Service } from "../libs/decorator";
 import { UserRepository } from "../repository/user.repository";
+import { UserInfoResponse } from "#cds-models/cnma/type";
 
 @Service("LearningHubService")
 export class LearningHubService {
   constructor(private readonly userRepo: UserRepository) {}
 
-  async getUserInfo(userID: string) {
+  async getUserInfo(userID: string): Promise<UserInfoResponse> {
     const user = await this.userRepo.findUserByID(userID);
-    return user;
+
+    const res: UserInfoResponse = {
+      ID: user.ID,
+      fullName: user.fullName,
+      email: user.email,
+    };
+
+    return res;
   }
 }
